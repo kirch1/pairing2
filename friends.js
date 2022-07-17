9299ac178916dad9b2c62827e66760e3296c19ae
Jason Kirchman <kirchman.jason@icloud.com>
2022-07-17 13:36:21 -0500

    Log numbers under 60

--- friends.js
+++ friends.js
@@ -3,3 +3,9 @@ for (var i = 0; i < numbers.length; i++) {
   if (numbers[i] > 30) {
     console.log(numbers[i]);
   }
+}
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 60) {
+    console.log(numbers[i]);
+  }
+}
